946e46df2fb9e824c2cf0ff5a61a7d6e7889e47e
Pavel Artiev <arijev@bk.ru>
2024-11-03 19:34:11 +0300

    small changes for masks.py

--- src/masks.py
+++ src/masks.py
@@ -4,8 +4,9 @@ from typing import Union
 def get_mask_card_number(card_number: Union[int, str]) -> str:
     """Эта функция принимает на вход номер карты и возвращает ее маску."""
     str_card_number = str(card_number)
-    if str_card_number.isdigit() is True:
-        return f"{str_card_number[0:4]} {str_card_number[4:6]}** **** {str_card_number[-4:]}"
+    if str_card_number.isdigit():
+        mask_of_card = f"{str_card_number[0:4]} {str_card_number[4:6]}** **** {str_card_number[-4:]}"
+        return mask_of_card
     else:
         return "Неверный формат номера карты"
 
@@ -13,12 +14,13 @@ def get_mask_card_number(card_number: Union[int, str]) -> str:
 def get_mask_account(account_number: Union[int, str]) -> str:
     """Эта функция принимает на вход номер счета и возвращает его маску."""
     str_account_number = str(account_number)
-    if str_account_number.isdigit() is True:
-        return f"**{str_account_number[-4:]}"
+    if str_account_number.isdigit():
+        mask_of_account = f"**{str_account_number[-4:]}"
+        return mask_of_account
     else:
         return "Неверный формат номера счета"
 
 
 if __name__ == "__main__":
-    print(get_mask_card_number(int(input("Впишите номер карты: "))))
-    print(get_mask_account(int(input("Впишите номер счета: "))))
+    print(get_mask_card_number(str(input("Впишите номер карты: "))))
+    print(get_mask_account('73654108430135874305'))

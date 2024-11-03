149f8cc5c3403410d765fdc156b55293020f0d35
Pavel Artiev <arijev@bk.ru>
2024-11-03 23:15:53 +0300

    add func get_date in widget.py

--- src/widget.py
+++ src/widget.py
@@ -22,6 +22,12 @@ def mask_account_card(card_or_account_number: str) -> str:
         return f'{card_or_account_number[0:-16]}{number}'
 
 
+def get_date(date: str) -> str:
+    """Функция форматирует и возвращает дату в виде ДД.ММ.ГГГГ."""
+    return f'{date[8:10]}.{date[5:7]}.{date[0:4]}'
+
+
 if __name__ == "__main__":
     print(mask_account_card('Счет 73654108430135874305'))
     print(mask_account_card('Visa Platinum 7000792289606361'))
+    print(get_date("2024-03-11T02:26:18.671407"))
